bf9c33b5fe34a9ef824befe41633ce7670496365
Viktor Brikalov <vbrikalov1989@mail.ru>
2021-08-03 11:59:04 +0300

    adding create request handling

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,19 +14,30 @@ const generateAccessToken = (id, email) => {
 class UserController {
   async create(req, res) {
     try {
-      const { fullName, password, birthday, email } = req.body;
-      const candidate = await User.findOne({where: {email}});
+      const {fullName, email, password, birthday} = req.body;
+      const emailNormalize = email.toLowerCase();
+      const candidate = await User.findOne({where: {email: emailNormalize}});
       if (candidate) {
-        return res.status(400).json({message: 'Пользователь с таким именем уже существует'});
+        return res.status(400).json({message: 'Пользователь с таким email уже существует'});
       }
+
+      if (password.length < 5) return res.status(400).json({message: 'Пароль не может быть менее 5 символов'});
+      if (fullName.length < 3) return res.status(400).json({message: 'Имя не может быть менее 3 символов'});
+
+      const regularExpEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      if (!regularExpEmail.test(String(email).toLowerCase())) 
+        return res.status(400).json({message: 'Неправильный формат email'});
+
+      if (!Date.parse(birthday)) return res.status(400).json({message: 'Неправильный формат даты'});
+
       const hashPassword = createHmac('sha256', salt).update(password).digest('hex');
       await User.create({
         fullName,
         password: hashPassword,
-        email,
+        email: emailNormalize,
         birthday,
       });
-      res.status(200).json('user has been added');
+      res.status(200).json('Пользователь успешно добавлен');
     }
     catch(e) {
       console.log(e);
@@ -118,7 +129,7 @@ class UserController {
         email: emailNormalize,
         birthday,
       });
-      res.status(200).json('user has been added');
+      res.status(200).json('Пользователь успешно добавлен');
     } catch (e) {
       console.log(e);
     }
@@ -129,11 +140,11 @@ class UserController {
       const {email, password} = req.body;
       const user = await User.findOne({where: {email}});
       if (!user) {
-        return res.status(400).json({message: 'пользователь не найден'});
+        return res.status(400).json({message: 'Пользователь не найден'});
       }
       const validPassword = createHmac('sha256', salt).update(password).digest('hex') === user.password;
       if (!validPassword) {
-        return res.status(400).json({message: 'введен неверный пароль'});
+        return res.status(400).json({message: 'Введен неверный пароль'});
       }
       const token = generateAccessToken(user.id, user.email);
       return res.status(200).json(token);
